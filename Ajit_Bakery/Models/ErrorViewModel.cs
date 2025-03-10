8f25d2c40aac523322acc7eccd548aa5ea9468a7
Dhiraj <Dhiraj@DESKTOP-VFU321B>
2025-03-10 17:00:18 +0530

    Add project files.

---
+++ Ajit_Bakery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ajit_Bakery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
