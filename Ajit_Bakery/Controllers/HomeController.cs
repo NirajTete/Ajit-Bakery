0f3975bc1ef6ef86d303cffd422d0d7e93154b82
Dhiraj <Dhiraj@DESKTOP-VFU321B>
2025-03-11 10:27:26 +0530

    Login Page

--- Ajit_Bakery/Controllers/HomeController.cs
+++ Ajit_Bakery/Controllers/HomeController.cs
@@ -18,6 +18,11 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult test()
+    {
+        return View();
+    }
+
     public IActionResult Privacy()
     {
         return View();
